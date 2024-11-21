45f06150f245d87a9fcc7916512131a89998e0c8
Lucas McFarlane <126113690+lucasjmcfarlane@users.noreply.github.com>
2024-11-21 15:45:20 -0600

    Add Javascript for the homepage to implement carousel functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
-//JavaScript
+window.onload = function () {
+    slide2 = document.getElementById("slide1");
+    slide2 = document.getElementById("slide2");
+    slide3 = document.getElementById("slide3");
+    slide1.style.display = "flex";
+    slide2.style.display = "none";
+    slide3.style.display = "none";
+}
+
+function showSlide(index) {
+    slide1 = document.getElementById("slide1");
+    slide2 = document.getElementById("slide2");
+    slide3 = document.getElementById("slide3");
+
+    slide1.style.display = "none";
+    slide2.style.display = "none";
+    slide3.style.display = "none";
+
+    switch (index) {
+        case 1:
+            slide1.style.display = "flex";
+            break;
+        case 2:
+            slide2.style.display = "flex";
+            break;
+        case 3:
+            slide3.style.display = "flex";
+            break;
+        default:
+            slide1.style.display = "flex";
+            break;
+    }
+
+
+
+
+}
