4916f3c8e885b054f07c5a2d52e5e5238a8164b9
Lucas McFarlane <126113690+lucasjmcfarlane@users.noreply.github.com>
2024-11-23 21:39:04 -0600

    Refactor popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,21 @@
 
-const dialog = document.getElementById("popup-dialog");
 
-function openPopUp(_src, _alt) {
-    
-    //set the image
+function openPopUp(popupImagePath, popupTitle, popupDescription) {
+    const dialog = document.getElementById("popup-dialog");
+
     const image = document.getElementById("popup-image");
-    image.src = _src;
+    image.src = popupImagePath;
 
     const title = document.getElementById("popup-title");
-    title.innerHTML = _alt
-    
+    title.innerHTML = popupTitle
+
+    const description = document.getElementById("popup-description");
+    description.innerHTML = popupDescription
+
     dialog.showModal();
 }
 
 function closePopUp() {
+    const dialog = document.getElementById("popup-dialog");
     dialog.close();
 }
