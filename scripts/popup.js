72bdb1d14cd47d5a917937e61642d9df9fd73160
K2bit <kurt20galvez@gmail.com>
2024-11-28 15:32:11 -0600

    another popup commit

--- scripts/popup.js
+++ scripts/popup.js
@@ -23,7 +23,7 @@ function closePopup(dialogElement) {
         var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
           rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
         if (!isInDialog) {
-    dialog.close();
-}
+          dialog.close();
+        }
       });
 }
