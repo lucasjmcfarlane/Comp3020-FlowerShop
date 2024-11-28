d27d9d36a69f1656e329ecd22507dbeab6cd38a3
Olufunmilayo Akintola <akintolo@myumanitoba.ca>
2024-11-27 19:19:55 -0600

    Changes made in navbar, homepage and customize lay out

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,8 +1,24 @@
 
+const dialog = document.getElementById("popup-dialog");
 
-function openPopUp(popupImagePath, popupTitle, popupDescription) {
-    const dialog = document.getElementById("popup-dialog");
+function openAndSelect(element) {
+    // Handle opening the popup
+    const imgElement = element.querySelector('img');
+    openPopUp(imgElement.src, imgElement.alt);
 
+    // Remove the "selected" class from all flower boxes
+    document.querySelectorAll('.flower-box').forEach((box) => {
+        box.classList.remove('selected');
+    });
+
+    // Add the "selected" class to the clicked flower box
+    element.classList.add('selected');
+}
+
+
+function openPopUp(_src, _alt) {
+    
+    //set the image
     const image = document.getElementById("popup-image");
     image.src = popupImagePath;
 
